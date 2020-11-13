56e73a58934d3dcfd14729d54b0160a804cb4aa7
Andrew Baker <andrew_j_baker2@hotmail.com>
2020-11-13 19:12:17 +0000

    Initial Text example

---
+++ examples/Text/Text.ino
@@ -0,0 +1,46 @@
+#include "NovaVGA.h"
+
+#define CS_PIN 10
+
+#define SCREEN_CHAR_WIDTH  NovaVGA.SCREEN_WIDTH  / NovaVGA.CHAR_WIDTH
+#define SCREEN_CHAR_HEIGHT NovaVGA.SCREEN_HEIGHT / NovaVGA.CHAR_HEIGHT
+
+int row = 0;
+int col = 0;
+
+void setup() {
+  NovaVGA.init(CS_PIN);
+
+  NovaVGA.fillScreen(NovaVGA.Black);
+
+  NovaVGA.drawString("Hello\nWorld", 0, 0, NovaVGA.Green);
+
+  row = NovaVGA.CHAR_HEIGHT * 3;
+
+#if !defined(ARDUINO)
+  NovaVGA.renderPresent();
+#endif
+}
+
+void loop() {
+  // XXX ...
+
+  // do nothing
+}
+
+#if !defined(ARDUINO)
+#include <iostream> // cin, getline()
+#include <cstdlib>  // EXIT_SUCCESS
+#include <string>   // string
+
+int main(int argc, char *argv[]) {
+  std::string s;
+
+  setup();
+  loop();
+  std::getline(std::cin, s);
+  NovaVGA.quit();
+  // XXX unreached
+  return EXIT_SUCCESS;
+}
+#endif
--- mk.sh
+++ mk.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 g++ -o examples/ColorPalette/ColorPalette -x c++ examples/ColorPalette/ColorPalette.ino SDL2NovaVGA.cpp -I. -D_GNU_SOURCE=1 -D_REENTRANT -lSDL2 -Wall
 g++ -o examples/Stripes/Stripes -x c++ examples/Stripes/Stripes.ino SDL2NovaVGA.cpp -I. -D_GNU_SOURCE=1 -D_REENTRANT -lSDL2 -Wall
+g++ -o examples/Text/Text -x c++ examples/Text/Text.ino SDL2NovaVGA.cpp -I. -D_GNU_SOURCE=1 -D_REENTRANT -lSDL2 -Wall
